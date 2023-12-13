780b7b83fb510d7232d70544135463208de5e1bc
TomichArg <tomich20@gmail.com>
2023-12-13 15:02:25 +0100

    botones next-prev

--- js/docs.js
+++ js/docs.js
@@ -1,17 +1,9 @@
 // Configuración:
-const capFolder = "capitulos";                                          // directorio de capitulos del manual
-const urlIndexManual = `${capFolder}/index.json`;                       // archivo JSON de indice del manual
-const manualGitHub = "https://github.com/TomasCEI/manual-md";           // URL del repositorio del manual en GitHub
-
+const capFolder = "capitulos"; // directorio de capitulos del manual
+const urlIndexManual = `${capFolder}/index.json`; // archivo JSON de indice del manual
+const manualGitHub = "https://github.com/TomasCEI/manual-md"; // URL del repositorio del manual en GitHub
 
 const contentDiv = document.getElementById("content");
-
-
-
-
-
-
-
 
 // ------------------  MENU DE CAPITULOS  ------------------ //
 
@@ -21,62 +13,59 @@ const contentDiv = document.getElementById("content");
 let A_ListaCapitulos = {};
 
 function crearMenuCapitulos(manual) {
-    const menuCapitulos = document.getElementById('menuCapitulos');
-    manual.capitulos.forEach((capitulo, index) => {
-        const capituloItem = document.createElement('li');
-        capituloItem.classList.add('Nav-capitulo');
-        capituloItem.innerHTML = `<a href="?cap=${capitulo.slug}">${capitulo.titulo}</a>`;
+  const menuCapitulos = document.getElementById("menuCapitulos");
+  manual.capitulos.forEach((capitulo, index) => {
+    const capituloItem = document.createElement("li");
+    capituloItem.classList.add("Nav-capitulo");
+    capituloItem.innerHTML = `<a href="?cap=${capitulo.slug}">${capitulo.titulo}</a>`;
 
-        if (capitulo.ficheros.length > 0) {
-            const subMenu = document.createElement('ul');
-            subMenu.classList.add('Nav-articulos');
-            capitulo.ficheros.forEach((fichero) => {
-                const ficheroItem = document.createElement('li');
-                ficheroItem.classList.add('Nav-articulo');
-                ficheroItem.innerHTML = `<a href="?cap=${capitulo.slug}#${fichero.slug}">${fichero.titulo}</a>`;
-                subMenu.appendChild(ficheroItem);
-            });
-            capituloItem.appendChild(subMenu);
-        }
-        menuCapitulos.appendChild(capituloItem);
-    });
+    if (capitulo.ficheros.length > 0) {
+      const subMenu = document.createElement("ul");
+      subMenu.classList.add("Nav-articulos");
+      capitulo.ficheros.forEach((fichero) => {
+        const ficheroItem = document.createElement("li");
+        ficheroItem.classList.add("Nav-articulo");
+        ficheroItem.innerHTML = `<a href="?cap=${capitulo.slug}#${fichero.slug}">${fichero.titulo}</a>`;
+        subMenu.appendChild(ficheroItem);
+      });
+      capituloItem.appendChild(subMenu);
+    }
+    menuCapitulos.appendChild(capituloItem);
+  });
 }
 
 fetch(urlIndexManual)
-    .then(response => response.json())
-    .then(datosJson => {
+  .then((response) => response.json())
+  .then((datosJson) => {
+    // almaceno únicamente los capitulos dentro de A_listaCapitulos
+    A_ListaCapitulos = datosJson.capitulos;
 
-        // almaceno únicamente los capitulos dentro de A_listaCapitulos
-        A_ListaCapitulos = datosJson.capitulos;
-
-        // Llamada a la función con tu estructura JSON
-        crearMenuCapitulos(datosJson);
-    }).then(() => {
-        imprimirTarjetas();
-    })
-    .catch(error => console.error('Error al obtener el archivo JSON:', error));
+    // Llamada a la función con tu estructura JSON
+    crearMenuCapitulos(datosJson);
+  })
+  .then(() => {
+    imprimirTarjetas();
+  })
+  .catch((error) => console.error("Error al obtener el archivo JSON:", error));
 
 // ------------------  FIN MENU DE CAPITULOS  ------------------ //
-
-
-
 
 // ------------------ NAVEGACION POR URL ------------------ //
 
 // Supongamos que la URL es algo así: "index.html?cap=capitulo1#seccion-25"
 const urlParams = new URLSearchParams(window.location.search);
-const capituloSeleccionado = urlParams.get('cap') || 'Bienvenida';
+const capituloSeleccionado = urlParams.get("cap") || "Bienvenida";
 
 function mostrarCapitulo(capitulo) {
-    // Lógica para mostrar el capítulo en la interfaz de usuario
-    console.log(`Mostrando el capítulo: ${capitulo}`);
+  // Lógica para mostrar el capítulo en la interfaz de usuario
+  console.log(`Mostrando el capítulo: ${capitulo}`);
 }
 
 // Escuchar cambios en la URL
-window.addEventListener('popstate', function () {
-    console.log("hice popstate cambie de página");
-    const capituloSeleccionado = urlParams.get('cap') || 'Bienvenida';
-    mostrarCapitulo(capituloSeleccionado);
+window.addEventListener("popstate", function () {
+  console.log("hice popstate cambie de página");
+  const capituloSeleccionado = urlParams.get("cap") || "Bienvenida";
+  mostrarCapitulo(capituloSeleccionado);
 });
 
 // Mostrar el capítulo inicial al cargar la página
@@ -84,121 +73,133 @@ mostrarCapitulo(capituloSeleccionado);
 
 // ------------------ FIN NAVEGACION POR URL ------------------ //
 
-
-
-
-
 // Función para realizar una solicitud de archivo Markdown
 const fetchMarkdown = async (archivo) => {
-    console.log("fetching: " + capituloSeleccionado + archivo.fichero);
-    return fetch(`${capFolder}/${capituloSeleccionado}/${archivo.fichero}`)
+  console.log("fetching: " + capituloSeleccionado + archivo.fichero);
+  return (
+    fetch(`${capFolder}/${capituloSeleccionado}/${archivo.fichero}`)
+      // respuesta standard
+      //.then(response => response.text())
 
-        // respuesta standard
-        //.then(response => response.text())
+      // respuesta con catch de error
+      .then((response) => {
+        if (!response.ok) {
+          //throw new Error("Fileeeee not found or fetch failed");
+          return (
+            "<span class='text-danger'>Fichero: <b>" +
+            archivo +
+            "</b> no encontrado</span>"
+          );
+        }
+        return response.text();
+      })
 
-        // respuesta con catch de error
-        .then(response => {
-            if (!response.ok) {
-                //throw new Error("Fileeeee not found or fetch failed");
-                return "<span class='text-danger'>Fichero: <b>" + archivo + "</b> no encontrado</span>";
-            }
-            return response.text();
-        })
+      // markdown standard
+      //.then(textoMarkdown => marked.parse(textoMarkdown))
 
-        // markdown standard
-        //.then(textoMarkdown => marked.parse(textoMarkdown))
+      // markdown con syntax highlighting
+      .then((textoMarkdown) => {
+        const htmlWithSyntaxHighlighting = marked.parse(textoMarkdown, {
+          langPrefix: "language-",
+        });
+        // Use Prism to apply syntax highlighting based on the language
+        const container = document.createElement("div");
+        container.innerHTML = htmlWithSyntaxHighlighting;
 
-        // markdown con syntax highlighting
-        .then(textoMarkdown => {
-            const htmlWithSyntaxHighlighting = marked.parse(textoMarkdown, { langPrefix: 'language-' });
-            // Use Prism to apply syntax highlighting based on the language
-            const container = document.createElement('div');
-            container.innerHTML = htmlWithSyntaxHighlighting;
-
-            container.querySelectorAll('pre code').forEach((codeBlock) => {
-                const language = codeBlock.className.replace(/language-/, '');
-                //Prism.highlightElement(codeBlock, false, () => language);
-                Prism.highlightElement(codeBlock);
-            });
-            return {
-                slug: archivo.slug,
-                title: archivo.titulo,
-                content: container.innerHTML
-            };
-        })
-
+        container.querySelectorAll("pre code").forEach((codeBlock) => {
+          const language = codeBlock.className.replace(/language-/, "");
+          //Prism.highlightElement(codeBlock, false, () => language);
+          Prism.highlightElement(codeBlock);
+        });
+        return {
+          slug: archivo.slug,
+          title: archivo.titulo,
+          content: container.innerHTML,
+        };
+      })
+  );
 };
 
 // Function to add content before the pre element
 function addContentBeforePre() {
-    //const preElement = document.querySelectorAll('pre[class^="language-"]');
-    const preElement = document.querySelectorAll('pre[class*="language-"]'); // es un vector vacío o con items
-    //console.log("PRE es: ", preElement);
-    //if (!preElement) return console.log("no hay preee");
+  //const preElement = document.querySelectorAll('pre[class^="language-"]');
+  const preElement = document.querySelectorAll('pre[class*="language-"]'); // es un vector vacío o con items
+  //console.log("PRE es: ", preElement);
+  //if (!preElement) return console.log("no hay preee");
 
-    preElement.forEach(element => {
+  preElement.forEach((element) => {
+    // Get the class name(s) of the current element [language-html o language-css etc]
+    const classNames = Array.from(element.classList);
 
-        // Get the class name(s) of the current element [language-html o language-css etc]
-        const classNames = Array.from(element.classList);
+    // Log or use the class name(s) as needed
+    // console.log("Class name(s):", classNames[0]);
+    const txtClassName = classNames[0].replace(/language-/, "");
 
-        // Log or use the class name(s) as needed
-        // console.log("Class name(s):", classNames[0]);
-        const txtClassName = classNames[0].replace(/language-/, '');
+    //const content = document.createTextNode("Titleeee to be inserted before the element!");
+    const beforeElement = document.createElement("div");
+    beforeElement.classList.add("code-title");
+    //beforeElement.appendChild(content);
+    beforeElement.innerHTML = `<span class="bloque-title">Bloque <strong>${txtClassName}</strong></span> <button class="bloque-copy" title="Copiar código al portapapeles"><i class="fa fa-copy"></i> Copiar</button>`;
 
-        //const content = document.createTextNode("Titleeee to be inserted before the element!");
-        const beforeElement = document.createElement('div');
-        beforeElement.classList.add("code-title");
-        //beforeElement.appendChild(content);
-        beforeElement.innerHTML = `<span class="bloque-title">Bloque <strong>${txtClassName}</strong></span> <button class="bloque-copy" title="Copiar código al portapapeles">Copiar</button>`;
-
-        //console.log("encontre un pre", element, beforeElement);
-        element.parentNode.insertBefore(beforeElement, element);
-    });
-    //preElement.parentNode.insertBefore(beforeElement, preElement);
+    //console.log("encontre un pre", element, beforeElement);
+    element.parentNode.insertBefore(beforeElement, element);
+  });
+  //preElement.parentNode.insertBefore(beforeElement, preElement);
 }
 
-
-
-
 function imprimirTarjetas() {
+  console.log("Lista capis es:", A_ListaCapitulos);
 
-    console.log("Lista capis es:", A_ListaCapitulos);
+  // Realizar todas las solicitudes manteniendo el orden del fetch
+  //Promise.all(A_capitulos.map(fetchMarkdown))
+  //Promise.all(A_ListaCapitulos["css"].map(fetchMarkdown))
+  //Promise.all(A_ListaCapitulos[capituloSeleccionado].map(fetchMarkdown))
+  //Promise.all(A_ListaCapitulosTemp[capituloSeleccionado].map(fetchMarkdown))
 
-    // Realizar todas las solicitudes manteniendo el orden del fetch
-    //Promise.all(A_capitulos.map(fetchMarkdown))
-    //Promise.all(A_ListaCapitulos["css"].map(fetchMarkdown))
-    //Promise.all(A_ListaCapitulos[capituloSeleccionado].map(fetchMarkdown))
-    //Promise.all(A_ListaCapitulosTemp[capituloSeleccionado].map(fetchMarkdown))
+  let A_articulos = A_ListaCapitulos.find(
+    (chapter) => chapter.slug === capituloSeleccionado
+  );
+  A_articulos = A_articulos.ficheros;
 
-    let A_articulos = A_ListaCapitulos.find(chapter => chapter.slug === capituloSeleccionado);
-    A_articulos = A_articulos.ficheros;
+  console.log("A_articuslo es:", A_articulos);
 
-    console.log("A_articuslo es:", A_articulos);
+  Promise.all(A_articulos.map(fetchMarkdown))
 
-    Promise.all(A_articulos.map(fetchMarkdown))
+    // Procesar los resultados de las solicitudes
+    .then((A_HtmlArticulos) => {
+      console.log("card es: ", A_HtmlArticulos);
 
+      // aquí se puede agregar tanto el Nombre de la sección, como el id para los href!
+      // const articuloTitle = "title";//A_articulos.titulo;
+      // const articuloSlug = "Capítulo " + capituloSeleccionado;
 
-        // Procesar los resultados de las solicitudes
-        .then((A_HtmlArticulos) => {
-            console.log("card es: ", A_HtmlArticulos);
+      // Concatenar y mostrar el HTML generado en orden
+      //contentDiv.innerHTML = A_HtmlGenerado.join("");
 
-            // aquí se puede agregar tanto el Nombre de la sección, como el id para los href!
-            // const articuloTitle = "title";//A_articulos.titulo;
-            // const articuloSlug = "Capítulo " + capituloSeleccionado;
+      //Crear divs con HTML generado y mostrar en orden
 
-            // Concatenar y mostrar el HTML generado en orden
-            //contentDiv.innerHTML = A_HtmlGenerado.join("");
+      A_HtmlArticulos.forEach((htmlArticulo, i) => {
+        const htmlGenerado = htmlArticulo.content;
+        const articuloTitle = htmlArticulo.title;
+        const articuloSlug = htmlArticulo.slug;
+        // const articuloPrevSlug = articuloSlug; // proximamente
+        // const articuloNextSlug = articuloSlug; // proximamente
+        let articuloPrevSlug = A_HtmlArticulos[i - 1]?.slug;
+        let articuloNextSlug = A_HtmlArticulos[i + 1]?.slug;
 
-            //Crear divs con HTML generado y mostrar en orden
+        const articuloPrevSlugBtn = articuloPrevSlug
+          ? `<a href="#${articuloPrevSlug}" class="Card-btn Card-btn--primary"> ${
+              A_HtmlArticulos[i - 1]?.title
+            }</a>`
+          : "";
+        const articuloNextSlugBtn = articuloNextSlug
+          ? `<a href="#${articuloNextSlug}" class="Card-btn Card-btn--secondary"> ${
+              A_HtmlArticulos[i + 1]?.title
+            }</a>`
+          : "";
 
-            A_HtmlArticulos.forEach(htmlArticulo => {
-                const htmlGenerado = htmlArticulo.content;
-                const articuloTitle = htmlArticulo.title;
-                const articuloSlug = htmlArticulo.slug;
-                const articuloPrevSlug = articuloSlug; // proximamente
-                const articuloNextSlug = articuloSlug; // proximamente
-                const readTime = Math.round(htmlGenerado.length / 1000);
-                const customHTML = `<div class="Card" id="${articuloSlug}">
+        const readTime = Math.round(htmlGenerado.length / 1000);
+        const customHTML = `<div class="Card" id="${articuloSlug}">
                                         <div class="Card-Header">
                                             <h1 class="Card-title">${articuloTitle}</h1>
                                             <div class="Card-Stats">
@@ -222,23 +223,23 @@ function imprimirTarjetas() {
                                                 </label>
                                             </form>
                                             <div class="Card-buttons">
+                                                ${articuloPrevSlugBtn}
+                                                ${articuloNextSlugBtn}
+                                                <!--
                                                 <a href="#${articuloPrevSlug}" class="Card-btn Card-btn--primary"></a>
                                                 <a href="#${articuloNextSlug}" class="Card-btn Card-btn--secondary"></a>
+                                                -->
                                             </div>
                                         </div>
                                     </div>`;
-                contentDiv.innerHTML += customHTML;
-            });
-            addContentBeforePre();
-        })
-        .catch(error => {
-            console.error("Error fetching or processing Markdown:", error);
-        });
+        contentDiv.innerHTML += customHTML;
+      });
+      addContentBeforePre();
+    })
+    .catch((error) => {
+      console.error("Error fetching or processing Markdown:", error);
+    });
 }
-
-
-
-
 
 // versión simplificada de obtener documentos con fetch.
 // Lista de capitulos Markdown
@@ -257,13 +258,5 @@ function imprimirTarjetas() {
 //     });
 // });
 
-
-
 // Obtener lista de Capitulos (html o md) y mostrarlos en orden
-document.addEventListener("DOMContentLoaded", function () {
-
-
-
-
-
-});
+document.addEventListener("DOMContentLoaded", function () {});
